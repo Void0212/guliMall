ecefb09d1bc2d4096c53c9ba3ad81742cf748412
Void0212 <55026409+Void0212@users.noreply.github.com>
2023-12-12 11:37:00 +0800

    初始化

---
+++ gulimall-coupon/src/test/java/com/cxl/gulimall/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxl.gulimall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
